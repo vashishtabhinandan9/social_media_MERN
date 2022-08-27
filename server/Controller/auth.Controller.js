51d6792a35357ccf669cb87ddafb748e8c02a591
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-08-27 22:46:10 +0530

    created signin route with jwt token creation

--- server/Controller/auth.Controller.js
+++ server/Controller/auth.Controller.js
@@ -3,6 +3,10 @@ const router=express.Router();
 const bcrypt=require('bcryptjs')
 const User =require("../Model/user.Model")
 
+const jwt = require('jsonwebtoken');
+
+
+
 const signup=(req,res)=>{
 
     const {name,email,password,pic} = req.body 
@@ -60,12 +64,62 @@ const signup=(req,res)=>{
     })
 }
   
-       
-
 
 
 const signin=(req,res)=>{
 
+    const {
+        email,
+        password
+    } = req.body;
+
+    if(!email || !password){
+        return res.status(422).json({error:"please add email or password"})
+     }
+
+    User.findOne({
+        email: email
+    }).exec((error, savedUser) => {
+
+        if (error) {
+            console.log(error);
+
+            return res.status(500).json({
+                success: false,
+                message: "DB Error occurred. Contact your administrator"
+            });
+        }
+        
+        if (savedUser) {
+// console.log("-----------start-----------")
+            const isAuthenticated =  bcrypt.compare(password,savedUser.password);
+            if (isAuthenticated) {
+
+                const token = jwt.sign({_id:savedUser._id},process.env.JWT_SECRET_KEY)
+                const {_id,name,email,followers,following,pic} = savedUser
+                return res.json({token,user:{_id,name,email,followers,following,pic}})
+                 /**
+                 * Generally to generate token we pass hte mongodb_id. 
+                 * So we pass the id of that collection and not of that user .
+                 * it is agood practice .that what we have done above
+
+                 */
+              
+            } else {
+                return res.json({
+                    success: false,
+                    message: "User Login failed. Bad Authentication"
+                })
+            }
+
+        } else {
+        //    console.log("false");
+            return res.json({
+                success: false,
+                message: "User Email Does not exist."
+            });
+        }
+    })
 }
 
 module.exports={signup,signin};
