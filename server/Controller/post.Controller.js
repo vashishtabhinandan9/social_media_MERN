11305aeaf883af531ad47f23a235087a84b4835f
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-08-29 01:46:21 +0530

    finished get all post route and get only user post route

--- server/Controller/post.Controller.js
+++ server/Controller/post.Controller.js
@@ -3,6 +3,31 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const Post =  require('../Model/post.Model')
 
+const getallpost=(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")//thing that you wanat to fill with data from other scheam
+    //1st argument is what you wnat to be filled
+    //2nd argumnet is what will be filled, if emoty all the dat will be filled
+
+    //.populate("comments.postedBy","_id name")
+    .sort('-createdAt')
+    .then((posts)=>{
+        res.json({posts})
+    }).catch(err=>{
+        console.log(err)
+    })
+}
+
+const mypost=(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+}
 
 const create=(req,res)=>{
     const {title,body,pic} = req.body 
@@ -28,4 +53,4 @@ const create=(req,res)=>{
     })
 }
          
-module.exports={create};
+module.exports={create,getallpost,mypost};
--- server/Routes/post.Router.js
+++ server/Routes/post.Router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedIn =require("../Middleware/auth.Middleware")
-const {create}  = require('../Controller/post.Controller')
+const {create,getallpost,mypost}  = require('../Controller/post.Controller')
 
 
 router.post('/createpost',isLoggedIn,create)
-
+router.get('/allpost',isLoggedIn,getallpost)
+router.get('/mypost',isLoggedIn,mypost)
 module.exports=router;
