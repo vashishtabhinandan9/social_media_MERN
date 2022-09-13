942db0b01feef286d2f67474a53a33001192e704
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-09-13 22:47:20 +0530

    made profile page large screen

--- client/src/Pages/Profile.page.jsx
+++ client/src/Pages/Profile.page.jsx
@@ -23,7 +23,7 @@ images.map((i)=>{
 const ProfileSm=()=> {
   return (
     <>
-    <div className='flex mt-4 ml-8 w-inherit gap-8 '>
+    <div className='headingsm flex mt-4 ml-8 w-inherit gap-8 '>
         <div className='1st flex flex-col items-center gap-3'>
           <div className='img  h-28 w-28'>
             <img className='h-full w-full rounded-full' src='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
@@ -60,7 +60,46 @@ const ProfileSm=()=> {
   )
 }
 
+const ProfileLg=()=> {
+  return (
+    <>
+    <div className='headinglg flex  mt-4 ml-8 w-full  gap-8 '>
+        
+          <div className='1st h-40 w-40 '>
+            <img className='h-full w-full rounded-full' src='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
+              alt='profile picture'/>
+          </div>
+       
 
+        <div className='2nd w-8/12 '>
+          <div className='bigusername  ' style={{fontFamily:'Segoe UI, Roboto, Helvetica, Arial, sans-serif'}}>
+            <h1 className='text-4xl mt-4 tracking-wide font-thin'>Alex Graham</h1>
+          </div>
+
+      <div className='statistics mt-8  '>
+           <ul className='flex  w-10/12 gap-8'>
+          <li className='cursor-pointer  text-center'> 40 Posts </li>
+          <li className='cursor-pointer  text-center'> 40 Followers </li>
+          <li className='cursor-pointer  text-center '> 40 Following </li>
+           </ul>
+      </div>
+
+          <div className='smallusername mt-4'>
+            <h2 className=" font-bold" style={{fontFamily:'Segoe UI, Roboto, Helvetica, Arial, sans-serif'}}>Alex Graham</h2>
+          </div>
+        </div>
+    </div>
+         
+
+    <div className='w-full border-t-2 border-gray-200 gallery grid grid-cols-3 gap-3 mt-4 pt-4 mx-30'>
+      <Gallery className=""/>
+      </div>
+   
+
+    
+    </>
+  )
+}
 const Profile = () => {
   return (
     <>
@@ -84,10 +123,10 @@ const Profile = () => {
         
         </div>
         
-        <div className="lgscreen hidden w-11/12 m-auto md:block">
+        <div className="lgscreen hidden w-9/12 m-auto md:block">
           {/*  laptoo and desktop screen greater than 1024 */}
           {/* <Nav /> */}
-          
+          <ProfileLg/>
         </div>
       </main>
     </>
